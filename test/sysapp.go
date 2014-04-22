45cc987d2e9ae40f7a543b81932e0368aae52a9b
Lance Burton <lance@animationmentor.com>
2014-04-22 12:23:37 -0700

    Barebones test application that adds a few simple items to a menu

---
+++ test/sysapp.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "flag"
+    "github.com/AnimationMentor/systray"
+    "github.com/golang/glog"
+    "os"
+    "path"
+    "path/filepath"
+    "runtime"
+)
+
+func main() {
+    //// Let us tap into the glog flags (like -alsologtostderr=true -log_dir=".")
+    //
+    
+    var iconDirFlag = flag.String("icon_path", ".", "Path to icons files")
+    var iconNameFlag = flag.String("icon_name", "sysapp.ico", "Name of icon to show")
+    flag.Parse()
+
+    var iconDir string
+    if filepath.IsAbs(*iconDirFlag) {
+        iconDir = *iconDirFlag
+    } else {
+        // Default our icon path to be relative to the exe
+        thisExe := os.Args[0]
+        thisDir := path.Dir(thisExe)
+        iconDir = path.Join(thisDir, *iconDirFlag)
+    }
+
+    tray := systray.New(iconDir, ".")
+
+    //// Set some test menu items
+    items := make(map[string]func())
+    items["Test Menu 1"] = func() {
+        println("Got menu 1")
+    }
+    items["Test Menu 2"] = func() {
+        println("Got menu 2")
+    }
+    tray.AddSystrayMenuItems(items)
+
+    err := tray.Show(*iconNameFlag, "Sysapp Test")
+    if err != nil {
+        glog.Infoln(err.Error())
+    }
+
+    runtime.LockOSThread()
+    tray.Run()
+    runtime.UnlockOSThread()
+}
