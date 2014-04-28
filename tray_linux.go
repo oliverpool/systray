52ac97431f94afcf0cd8fa8839814e47cedb7291
Lance Burton <lance@animationmentor.com>
2014-04-28 16:35:52 -0700

    Stub in enough to compile with new API

--- tray_linux.go
+++ tray_linux.go
@@ -1,9 +1,48 @@
 package systray
 
-func _NewSystray(iconPath string, clientPath string, port int) *_Systray {
-	return &_Systray{_NewSystraySvr(iconPath, clientPath, port)}
+func _NewSystray(iconPath string, clientPath string) *_Systray {
+	return &_Systray{}
 }
 
 type _Systray struct {
-	*_SystraySvr
+	lclick func()
+	rclick func()
+	dclick func()
+}
+
+func (p *_Systray) Show(file string, hint string) error {
+	return nil
+}
+
+func (p *_Systray) Stop() error {
+	return nil
+}
+
+func (p *_Systray) SetIcon(file string) error {
+	return nil
+}
+
+func (p *_Systray) SetTooltip(tooltip string) error {
+	return nil
+}
+
+func (p *_Systray) SetVisible(visible bool) error {
+	return nil
+}
+
+func (p *_Systray) Run() error {
+	return nil
+}
+
+func (p *_Systray) OnClick(fun func()) {
+    p.lclick = fun
+    p.rclick = fun
+    p.dclick = fun
+}
+
+
+func (p *_Systray) ClearSystrayMenuItems() {
+}
+
+func (p *_Systray) AddSystrayMenuItems(items []CallbackInfo) {
 }
